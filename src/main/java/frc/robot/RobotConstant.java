bb711c7b24d8934308708e5782aef7f5752cd6f9
mickjosh <felix_roux@hotmail.com>
2023-01-22 01:07:31 -0500

    Adding Robot Constant

---
+++ src/main/java/frc/robot/RobotConstant.java
@@ -0,0 +1,43 @@
+package frc.robot;
+
+import frc.robot.Library.FRC_3117_Tools.Component.Data.MotorController.MotorControllerType;
+import frc.robot.Library.FRC_3117_Tools.Math.Vector2d;
+
+public class RobotConstant 
+{
+    public static final MotorControllerType SWERVE_FRONT_LEFT_STEER_TYPE = MotorControllerType.SparkMax;
+    public static final int SWERVE_FRONT_LEFT_STEER = 0;
+    public static final int SWERVE_FRONT_LEFT_STEER_ENCODER = 0;
+    public static final double SWERVE_FRONT_LEFT_STEER_ENCODER_OFFSET = 0;
+    public static final MotorControllerType SWERVE_FRONT_LEFT_DRIVE_TYPE = MotorControllerType.TalonFX;
+    public static final int SWERVE_FRONT_LEFT_DRIVE = 0;
+    public static final Vector2d SWERVE_FRONT_LEFT_POSITION = new Vector2d();
+
+    public static final MotorControllerType SWERVE_FRONT_RIGHT_STEER_TYPE = MotorControllerType.SparkMax;
+    public static final int SWERVE_FRONT_RIGHT_STEER = 0;
+    public static final int SWERVE_FRONT_RIGHT_STEER_ENCODER = 0;
+    public static final double SWERVE_FRONT_RIGHT_STEER_ENCODER_OFFSET = 0;
+    public static final MotorControllerType SWERVE_FRONT_RIGHT_DRIVE_TYPE = MotorControllerType.TalonFX;
+    public static final int SWERVE_FRONT_RIGHT_DRIVE = 0;
+    public static final Vector2d SWERVE_FRONT_RIGHT_POSITION = new Vector2d();
+
+    public static final MotorControllerType SWERVE_REAR_LEFT_STEER_TYPE = MotorControllerType.SparkMax;
+    public static final int SWERVE_REAR_LEFT_STEER = 0;
+    public static final int SWERVE_REAR_LEFT_STEER_ENCODER = 0;
+    public static final double SWERVE_REAR_LEFT_STEER_ENCODER_OFFSET = 0;
+    public static final MotorControllerType SWERVE_REAR_LEFT_DRIVE_TYPE = MotorControllerType.TalonFX;
+    public static final int SWERVE_REAR_LEFT_DRIVE = 0;
+    public static final Vector2d SWERVE_REAR_LEFT_POSITION = new Vector2d();
+
+    public static final MotorControllerType SWERVE_REAR_RIGHT_STEER_TYPE = MotorControllerType.SparkMax;
+    public static final int SWERVE_REAR_RIGHT_STEER = 0;
+    public static final int SWERVE_REAR_RIGHT_STEER_ENCODER = 0;
+    public static final double SWERVE_REAR_RIGHT_STEER_ENCODER_OFFSET = 0;
+    public static final MotorControllerType SWERVE_REAR_RIGHT_DRIVE_TYPE = MotorControllerType.TalonFX;
+    public static final int SWERVE_REAR_RIGHT_DRIVE = 0;
+    public static final Vector2d SWERVE_REAR_RIGHT_POSITION = new Vector2d();
+
+    public static final String SWERVE_TRANSLATON_X_AXIS = "Horizontal";
+    public static final String SWERVE_TRANSLATION_Y_AXIS = "Vertical";
+    public static final String SWERVE_ROTATION_AXIS = "Rotation";
+}
