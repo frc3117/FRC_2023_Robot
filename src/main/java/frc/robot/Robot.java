24ea6d8ab1ae2cc6fc5e2bc86ad5419975d0d42f
mickjosh <felix_roux@hotmail.com>
2023-02-22 21:08:35 -0500

    Move Swerve To Manifest

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,13 +5,7 @@ import frc.robot.Library.FRC_3117_Tools.Math.Vector3d;
 import frc.robot.Library.FRC_3117_Tools.RobotBase;
 import frc.robot.Library.FRC_3117_Tools.Component.Swerve;
 import frc.robot.Library.FRC_3117_Tools.Component.Data.Input;
-import frc.robot.Library.FRC_3117_Tools.Component.Data.WheelData;
-import frc.robot.Library.FRC_3117_Tools.Component.Data.Input.XboxAxis;
-import frc.robot.Library.FRC_3117_Tools.Component.Swerve.DrivingMode;
-import frc.robot.Library.FRC_3117_Tools.Wrapper.ADIS16448_IMU_Gyro;
-import frc.robot.Library.FRC_3117_Tools.Wrapper.Encoder.AnalogAbsoluteEncoder;
 import frc.robot.Library.FRC_3117_Tools.Wrapper.Encoder.DutyCycleAbsoluteEncoder;
-import frc.robot.RobotConstant.RobotConstant;
 import frc.robot.SubSystems.Data.ManipulatorSegmentData;
 import frc.robot.SubSystems.Manipulator;
 import frc.robot.SubSystems.Data.ManipulatorData;
@@ -93,56 +87,14 @@ public class Robot extends RobotBase
   @Override
   public void CreateComponentInstance()
   {
-    // Front Left
-    var swerveModuleFL = new WheelData();
-    swerveModuleFL.DriveController = RobotConstant.SWERVE_FRONT_LEFT_DRIVE_CONTROLLER.ToMotorController();
-    swerveModuleFL.DirectionController = RobotConstant.SWERVE_FRONT_LEFT_STEER_CONTROLLER.ToMotorController();
-    swerveModuleFL.DirectionEncoder = new AnalogAbsoluteEncoder(RobotConstant.SWERVE_FRONT_LEFT_STEER_ENCODER);
-    swerveModuleFL.WheelPosition = RobotConstant.SWERVE_FRONT_LEFT_POSITION;
-    swerveModuleFL.AngleOffset = RobotConstant.SWERVE_FRONT_LEFT_STEER_ENCODER_OFFSET;
-
-    // Front Right
-    var swerveModuleFR = new WheelData();
-    swerveModuleFR.DriveController = RobotConstant.SWERVE_FRONT_RIGHT_DRIVE_CONTROLLER.ToMotorController();
-    swerveModuleFR.DirectionController = RobotConstant.SWERVE_FRONT_RIGHT_STEER_CONTROLLER.ToMotorController();
-    swerveModuleFR.DirectionEncoder = new AnalogAbsoluteEncoder(RobotConstant.SWERVE_FRONT_RIGHT_STEER_ENCODER);
-    swerveModuleFR.WheelPosition = RobotConstant.SWERVE_FRONT_RIGHT_POSITION;
-    swerveModuleFR.AngleOffset = RobotConstant.SWERVE_FRONT_RIGHT_STEER_ENCODER_OFFSET;
-
-    // Rear Left
-    var swerveModuleRL = new WheelData();
-    swerveModuleRL.DriveController = RobotConstant.SWERVE_REAR_LEFT_DRIVE_CONTROLLER.ToMotorController();
-    swerveModuleRL.DirectionController = RobotConstant.SWERVE_REAR_LEFT_STEER_CONTROLLER.ToMotorController();
-    swerveModuleRL.DirectionEncoder = new AnalogAbsoluteEncoder(RobotConstant.SWERVE_REAR_LEFT_STEER_ENCODER);
-    swerveModuleRL.WheelPosition = RobotConstant.SWERVE_REAR_LEFT_POSITION;
-    swerveModuleRL.AngleOffset = RobotConstant.SWERVE_REAR_LEFT_STEER_ENCODER_OFFSET;
-
-    // Rear Left
-    var swerveModuleRR = new WheelData();
-    swerveModuleRR.DriveController = RobotConstant.SWERVE_REAR_RIGHT_DRIVE_CONTROLLER.ToMotorController();
-    swerveModuleRR.DirectionController = RobotConstant.SWERVE_REAR_RIGHT_STEER_CONTROLLER.ToMotorController();
-    swerveModuleRR.DirectionEncoder = new AnalogAbsoluteEncoder(RobotConstant.SWERVE_REAR_RIGHT_STEER_ENCODER);
-    swerveModuleRR.WheelPosition = RobotConstant.SWERVE_REAR_RIGHT_POSITION;
-    swerveModuleRR.AngleOffset = RobotConstant.SWERVE_REAR_RIGHT_STEER_ENCODER_OFFSET;
-
-    var swerveModules = new WheelData[] {
-      swerveModuleFL,
-      swerveModuleFR,
-      swerveModuleRL,
-      swerveModuleRR
-    };
-
-    var swerve = new Swerve(swerveModules, new ADIS16448_IMU_Gyro());
+    Swerve swerve = GetComponent("SwerveDrive");
 
     swerve.SetPIDGain(0, 0.5, 0, 0);
     swerve.SetPIDGain(1, 0.5, 0, 0);
     swerve.SetPIDGain(2, 0.5, 0, 0);
     swerve.SetPIDGain(3, 0.5, 0, 0);
 
-    swerve.SetCurrentMode(DrivingMode.World);
     swerve.SetHeadingOffset(Math.PI / 2);
-
-    AddComponent("Swerve", swerve);
     
     // Manipulator
     var manipulatorData = new ManipulatorData();
@@ -192,6 +144,7 @@ public class Robot extends RobotBase
   @Override
   public void CreateInput()
   {
+    /*
     //Swerve
     Input.CreateAxis("Horizontal", 0, XboxAxis.LEFTX, false);
     Input.CreateAxis("Vertical", 0, XboxAxis.LEFTY, true);
@@ -201,6 +154,6 @@ public class Robot extends RobotBase
 
     Input.SetAxisDeadzone("Horizontal", 0.15);
     Input.SetAxisDeadzone("Vertical", 0.15);
-    Input.SetAxisDeadzone("Rotation", 0.15);
+    Input.SetAxisDeadzone("Rotation", 0.15);*/
   }
 }
