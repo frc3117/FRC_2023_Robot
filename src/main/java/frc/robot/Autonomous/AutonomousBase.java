cff42cbc6997dfd9093bbc74231cd434b980d8b5
mickjosh <felix_roux@hotmail.com>
2023-03-16 09:52:53 -0400

    Add Autonomous Base

---
+++ src/main/java/frc/robot/Autonomous/AutonomousBase.java
@@ -0,0 +1,7 @@
+package frc.robot.Autonomous;
+
+public abstract class AutonomousBase {
+    public abstract void StartAuto();
+    public abstract void AutoLoop();
+    public abstract void AutoEnd();
+}
