17a43d8d2651efbc9ee1d8c19812868e7316ed90
mickjosh <felix_roux@hotmail.com>
2023-03-16 09:53:30 -0400

    Add Get Name to Autonomous Base

--- src/main/java/frc/robot/Autonomous/AutonomousBase.java
+++ src/main/java/frc/robot/Autonomous/AutonomousBase.java
@@ -1,6 +1,8 @@
 package frc.robot.Autonomous;
 
 public abstract class AutonomousBase {
+    public abstract String GetName();
+    
     public abstract void StartAuto();
     public abstract void AutoLoop();
     public abstract void AutoEnd();
